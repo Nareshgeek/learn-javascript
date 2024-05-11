efdd21ec78cf469685c72dcf2a6e9e75b51a4f66
Nareshgeek <44625271+Nareshgeek@users.noreply.github.com>
2024-05-11 06:47:47 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using nodejs, not browser 
+
+console.log("It is printing well");
+
+let name = "Naresh"
+let age = 18
+let isLoggedIn = true
+let city;
+
+// Data Types
+ 
+// number => 2 powerof 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value // object
+// undefined
+// symbol => unique
+
+console.log(typeof null);
+console.log(typeof city);d
